8aa064fa967547b7d06220a92a41e6b35945ad3e
Sigmaflo2 <kjschocolate@gmail.com>
2018-11-23 20:42:51 +0900

    main 내용 추가

--- Calculator.java
+++ Calculator.java
@@ -1,13 +1,26 @@
-public class Calculator {
-    public int add(int a, int b) {
-      return a + b;
-    }
+public class Calculator(){
+  public static void main(String[] args){
+    int a = 4;
+    int b = 2;
+    System.out.println(sum(a,b));
+    System.out.println(minus(a,b));
+    System.out.println(mul(a,b));
+    System.out.println(div(a,b));
+  }
 
-    public int sub(int a, int b) {
-      return a - b;
-    }
+  public int sum(int a, int b){
+    return a + b;
+  }
 
-    public int mux(int a, int b){
-      return a * b;
-    }
+  public int minus(int a, int b){
+    return a - b;
+  }
+
+  public int mul(int a, int b) {
+    return a * b;
+  }
+
+  public int div(int a, int b) {
+    return a / b;
+  }
 }
